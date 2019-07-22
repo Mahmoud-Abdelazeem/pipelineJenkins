2e323d94434cc1f96ac10473d1a11d7a386327f0
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-22 15:51:08 +0200

    add the required file

--- time_check.sh
+++ time_check.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ln -s $WORKSPACE/unittest example-pipeline/unittest/test-result.xml 
 
 cd ~/workspace/example-pipeline/unittest
 
 echo $PWD
  
-touch *test-result.xml
+touch test-result.xml
