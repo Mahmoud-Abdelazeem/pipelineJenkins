cc396857c34c93099f47bfe5ef4a54bfd91d074e
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-27 21:09:56 +0200

    add the file

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -10,10 +10,10 @@ cmake_minimum_required(VERSION 3.10.2)
     set (MY_LIB_TEST  testlib)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
 
-    #add_library(${MY_LIB_TEST} STATIC ${LIB_FOLDER})
-    #target_include_directories(${MY_LIB_TEST} PRIVATE ${LIB_FOLDER})
+    add_library(${MY_LIB_TEST} SHARED ${LIB_FOLDER})
+    target_include_directories(${MY_LIB_TEST} PRIVATE ${LIB_FOLDER})
    
-    #target_link_libraries(${MY_LIB_TEST}
-     #   PUBLIC ${BENCHTOP_LIB_TEST}
-      #  PUBLIC ${OTHER_LIB_TEST}
-       # )
+    target_link_libraries(${MY_LIB_TEST}
+        PUBLIC ${BENCHTOP_LIB_TEST}
+        PUBLIC ${OTHER_LIB_TEST}
+        )
