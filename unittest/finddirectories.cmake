13fef79726c28475079573b273f5d7249bbc484e
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-27 20:42:58 +0200

    add the required value

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -10,7 +10,7 @@ cmake_minimum_required(VERSION 3.10.2)
     set (MY_LIB_TEST  testlib)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
 
-    add_library(${MY_LIB_TEST} ${CMAKE_CURRENT_SOURCE_DIR}/mahTest/mahtest.cpp ${LIB_FOLDER})
+    add_library(${MY_LIB_TEST} STATIC ${LIB_FOLDER})
     target_include_directories(${MY_LIB_TEST} PRIVATE ${LIB_FOLDER})
    
     target_link_libraries(${MY_LIB_TEST}
