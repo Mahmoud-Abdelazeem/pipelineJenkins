6d505cc22c033e93182f13f5e8e518c80bb23b30
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-27 23:40:59 +0200

    the file

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -2,11 +2,11 @@ cmake_minimum_required(VERSION 3.10.2)
 
     set(LIB_FOLDER
         ~/Projects/qtpipeline/
-        )
+       )
         
+    link_directories (${LIB_FOLDER})
     include_directories(~/Projects/qtpipeline/)
     message (STATUS "Give me the Link ${LIB_FOLDER}")
-    #link_directories (${LIB_FOLDER})
     set (MY_LIB_TEST  testlib)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
 
@@ -14,6 +14,6 @@ cmake_minimum_required(VERSION 3.10.2)
     target_include_directories(${MY_LIB_TEST} PRIVATE ${LIB_FOLDER})
    
     target_link_libraries(${MY_LIB_TEST}
-        PUBLIC ${BENCHTOP_LIB_TEST}
-        PUBLIC ${OTHER_LIB_TEST}
-        )
+                          PUBLIC ${BENCHTOP_LIB_TEST}
+                          PUBLIC ${OTHER_LIB_TEST}
+                         )
