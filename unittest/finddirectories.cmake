ef05ef059c89920b763a91006243379f44e05a9c
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-29 11:26:27 +0200

    add the required files

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -2,12 +2,14 @@ cmake_minimum_required(VERSION 3.10.2)
 
     set(LIB_FOLDER
         ~/Projects/qtpipeline/
+        #for jenkins
         /home/mahmoud86/Projects/qtpipeline
        )
         
     link_directories (${LIB_FOLDER})
     include_directories(~/Projects/qtpipeline/)
     message (STATUS "Give me the Link ${LIB_FOLDER}")
+    # for Jenkins, must be changed 
     include_directories(/home/mahmoud86/Projects/qtpipeline)
     set (MY_LIB_TEST  testFolder)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
