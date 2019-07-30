efb59ad788dc1c522341981b0e85fc253ae44fe2
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-07-30 14:11:03 +0200

    finddirectories

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -14,11 +14,11 @@ cmake_minimum_required(VERSION 3.10.2)
     set (MY_LIB_TEST  testFolder)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
 
-    add_library(${MY_LIB_TEST} SHARED ${LIB_FOLDER})
+    add_library(${MY_LIB_TEST} STATIC ${LIB_FOLDER})
     target_include_directories(${MY_LIB_TEST} PRIVATE ${LIB_FOLDER})
    
     target_link_libraries(${MY_LIB_TEST}
                           PUBLIC ${BENCHTOP_LIB_TEST}
                           PUBLIC ${OTHER_LIB_TEST}
-                          PUBLIC ${DISTIMATIC_LIB_TEST}
+                         # PUBLIC ${DISTIMATIC_LIB_TEST}
                          )
