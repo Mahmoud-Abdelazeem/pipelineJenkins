9ea162d73d85b835a31e0b1cdd5c006028dc6495
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-08-01 15:50:23 +0200

    the build file

--- build_file.sh
+++ build_file.sh
@@ -2,6 +2,6 @@
 
 export PATH=$PATH:~/vcpkg/scripts/buildsystems/
 
-ln -s $WORKSPACE ~/vcpkg/scripts/buildsystems/
+ln -s  ~/vcpkg/scripts/buildsystems/ $WORKSPACE
 cmake arguments: '-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake', installation: 'InSearchPath'
                 cmakeBuild buildType: 'Release', cleanBuild: true, installation: 'InSearchPath', steps: [[withCmake:            true]]
