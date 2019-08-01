f296bca4628b5b1d360f9b343fcb05a95ca4b638
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-08-01 16:59:14 +0200

    the message

--- build_file.sh
+++ build_file.sh
@@ -1,7 +1,4 @@
 #!bin/bash
 
-export PATH=$PATH:~/vcpkg/
+#export PATH=$PATH:~/vcpkg/
 
-cmake arguments: '-DCMAKE_TOOLCHAIN_FILE=$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake', installation: 'InSearchPath'
-
-cmakeBuild buildType: 'Release', cleanBuild: true, installation: 'InSearchPath', steps: [[withCmake: true]]
