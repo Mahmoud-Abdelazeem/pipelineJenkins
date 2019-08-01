fb1faf18ef0c0e444aad0e0acdfcf46dcfedb67e
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-08-01 15:08:07 +0200

    add the files

--- build_file.sh
+++ build_file.sh
@@ -2,5 +2,4 @@
 
 export PATH=$PATH:~/vcpkg/scripts/buildsystems/
 
-cmake arguments: '-DCMAKE_TOOLCHAIN_FILE=vcpkg.cmake', installation: 'InSearchPath'
-                cmakeBuild buildType: 'Release', cleanBuild: true, installation: 'InSearchPath', steps: [[withCmake:  true]]    
+cmake
