f241a3e2ee43ea3100fe8ff8e09cc0956a7f43fd
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-08-01 16:15:43 +0200

    the file

--- build_file.sh
+++ build_file.sh
@@ -1,7 +1,5 @@
 #!bin/bash
 
+cmake arguments: '-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake', installation: 'InSearchPath'
 
- 
-cmake arguments: '-DCMAKE_TOOLCHAIN_FILE=/home/mahmoud86/vcpkg/scripts/buildsystems/vcpkg.cmake', installation: 'InSearchPath'
-
-cmakeBuild buildType: 'Release', cleanBuild: true, installation: 'InSearchPath', steps: [[withCmake:true]]
+cmakeBuild buildType: 'Release', cleanBuild: true, installation: 'InSearchPath', steps: [[withCmake: true]]
