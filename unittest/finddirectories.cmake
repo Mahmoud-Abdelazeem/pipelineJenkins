a27279dad0eb9c0982cd6124b1d31edc4adad85b
Mahmoud-Abdelazeem <ceng-mahmoud@hotmail.com>
2019-08-02 10:42:05 +0200

    add the directories

--- unittest/finddirectories.cmake
+++ unittest/finddirectories.cmake
@@ -9,7 +9,7 @@ cmake_minimum_required(VERSION 3.10.2)
         
     link_directories (${LIB_FOLDER})
     message (STATUS "Give me the Link ${LIB_FOLDER}")
-    include_directories(home/mahmoud86/Projects/qtpipeline/)
+    include_directories(/home/mahmoud86/Projects/qtpipeline/)
 
     set (MY_LIB_TEST  testFolder)
     message(STATUS "What is the LIB_TEST ${MY_LIB_TEST}")
